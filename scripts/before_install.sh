d8ad8568840d04b95326012c1e095f8e3be65cc6
Christian Okonta <okonta.christian@yahoo.com>
2025-08-15 00:06:58 -0400

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,10 @@ RELEASES_DIR="/home/ubuntu/iendorse/Back-API/releases"
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 NEW_RELEASE="$RELEASES_DIR/$TIMESTAMP"
 
+# Ensure parent folder exists and is writable
+mkdir -p "$RELEASES_DIR"
+chmod u+rwx "$RELEASES_DIR"
+
 echo "Creating new release folder: $NEW_RELEASE"
 mkdir -p "$NEW_RELEASE"
 
