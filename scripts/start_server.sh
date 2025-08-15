02943f629afb19dee26f5120f2e0e928d0fb1163
Christian Okonta <okonta.christian@yahoo.com>
2025-08-14 22:43:54 -0400

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,21 @@
+
+
+
 #!/bin/bash
+
 echo "Starting application..."
-cd /home/ubuntu/iendorse/Back-API/graphAPI
-pm2 start server.js --name iendorse-api || pm2 reload iendorse-api
+# Navigate to app directory
+cd /home/ubuntu/iendorse/Back-API/graphAPI || exit 1
+
+# Install dependencies just in case
+npm ci --omit=dev
+
+# Reload PM2 process if it exists, else start it
+if pm2 list | grep -q iendorse; then
+  pm2 reload iendorse
+else
+  pm2 start server.js --name iendorse
+fi
+
+# Save PM2 process list
 pm2 save
