f4a1825f98e5d7fe2d563b2d6d91fcf89ae594f8
Christian Okonta <okonta.christian@yahoo.com>
2025-08-14 23:37:23 -0400

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Stopping application..."
-pm2 stop iendorse || true
+set -e
+
+echo "Stopping PM2 process if it exists..."
+pm2 describe iendorse > /dev/null 2>&1 && pm2 stop iendorse || true
+echo "Stop server completed."
